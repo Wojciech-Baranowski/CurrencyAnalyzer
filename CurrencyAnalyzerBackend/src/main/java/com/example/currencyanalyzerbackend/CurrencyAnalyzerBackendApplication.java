5426295964d3d20a8a160298a610245e6de74fe7
simmondobber <wojtek55501000@wp.pl>
2022-04-15 18:40:32 +0200

    Initial commit

---
+++ CurrencyAnalyzerBackend/src/main/java/com/example/currencyanalyzerbackend/CurrencyAnalyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyanalyzerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAnalyzerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyAnalyzerBackendApplication.class, args);
+    }
+
+}
