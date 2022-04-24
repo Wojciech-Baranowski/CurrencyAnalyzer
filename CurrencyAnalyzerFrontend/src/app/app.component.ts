de1f7ccb28384a50357b00db40e78ff2b702df6f
simmondobber <wojtek55501000@wp.pl>
2022-04-24 03:59:46 +0200

    frontend refactor

--- CurrencyAnalyzerFrontend/src/app/app.component.ts
+++ CurrencyAnalyzerFrontend/src/app/app.component.ts
@@ -4,152 +4,151 @@ import {CurrencyResponseDto} from "./dtos/currency-response-dto";
 import {FormBuilder} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent implements OnInit{
 
-  private URL = 'http://localhost:8080/currency';
-  private data: CurrencyResponseDto;
+    private URL = 'http://localhost:8080/currency';
+    private data: CurrencyResponseDto;
 
-  error: boolean = true;
-  errorMessage: string = '';
+    error: boolean = true;
+    errorMessage: string = '';
 
-  filter = this.formBuilder.group({
-    currencyCode: 'USD',
-    startDate: this.getYearBeforeDate(),
-    endDate: this.getTodayDate(),
-  });
+    filter = this.formBuilder.group({
+        currencyCode: 'USD',
+        startDate: this.getYearBeforeDate(),
+        endDate: this.getTodayDate(),
+    });
 
-  bidTrace = {
-    x: [''],
-    y: [0],
-    mode: 'lines',
-    line: {
-      color: 'rgb(55, 128, 191)',
-      width: 1
-    },
-    name: 'bidPrice'
-  };
-
-  saleTrace = {
-    x: [''],
-    y: [0],
-    mode: 'lines',
-    line: {
-      color: 'rgb(191, 55, 55)',
-      width: 1
-    },
-    name: 'salePrice'
-  };
-
-  bidDifferenceTrace = {
-    x: [''],
-    y: [0],
-    mode: 'lines',
-    line: {
-      color: 'rgb(55, 128, 191)',
-      width: 0.6
-    },
-    name: 'bidPriceDifference'
-  };
-
-  saleDifferenceTrace = {
-    x: [''],
-    y: [0],
-    mode: 'lines',
-    line: {
-      color: 'rgb(191, 55, 55)',
-      width: 0.5
-    },
-    name: 'salePriceDifference'
-  };
-
-  layout = {
-    title: '',
-    xaxis: {
-      type: 'date'
-    },
-    yaxis: {
-      type: 'linear'
-    }
-  };
-
-  priceTraces = [this.bidTrace, this.saleTrace];
-  differenceTraces = [this.bidDifferenceTrace, this.saleDifferenceTrace];
-
-  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
-    this.data = new CurrencyResponseDto();
-  }
-
-  ngOnInit() {
-    this.getData();
-  }
-
-  onSubmit() {
-    this.getData();
-  }
-
-  private getData() {
-    let body = {
-      currencyCode: this.filter.controls['currencyCode'].value,
-      startDate: this.filter.controls['startDate'].value,
-      endDate: this.filter.controls['endDate'].value
+    bidTrace = {
+        x: [''],
+        y: [0],
+        mode: 'lines',
+        line: {
+            color: 'rgb(55, 128, 191)',
+            width: 1
+        },
+        name: 'bidPrice'
     };
-    this.http.post<CurrencyResponseDto>(`${this.URL}`, body)
-      .subscribe(data => {
-          this.data = data;
-          this.createPricePlotTraces();
-          this.createDifferencePlotTraces();
-          this.layout.title = this.data.name + ' (' + this.filter.controls['currencyCode'].value + ')';
-          this.error = false;
+
+    saleTrace = {
+        x: [''],
+        y: [0],
+        mode: 'lines',
+        line: {
+            color: 'rgb(191, 55, 55)',
+            width: 1
+        },
+        name: 'salePrice'
+    };
+
+    bidDifferenceTrace = {
+        x: [''],
+        y: [0],
+        mode: 'lines',
+        line: {
+            color: 'rgb(55, 128, 191)',
+            width: 0.6
+        },
+        name: 'bidPriceDifference'
+    };
+
+    saleDifferenceTrace = {
+        x: [''],
+        y: [0],
+        mode: 'lines',
+        line: {
+            color: 'rgb(191, 55, 55)',
+            width: 0.5
+        },
+        name: 'salePriceDifference'
+    };
+
+    layout = {
+        title: '',
+        xaxis: {
+            type: 'date'
+        },
+        yaxis: {
+            type: 'linear'
         }
-        ,(error) => {
-          this.error = true;
-          this.errorMessage = error.error;
-        });
-  }
+    };
 
-  private createPricePlotTraces(){
+    priceTraces = [this.bidTrace, this.saleTrace];
+    differenceTraces = [this.bidDifferenceTrace, this.saleDifferenceTrace];
 
-    this.bidTrace.x = this.data.records.map(record => record.date);
-    this.bidTrace.y = this.data.records.map(record => record.bidPrice);
+    constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+        this.data = new CurrencyResponseDto();
+    }
 
-    this.saleTrace.x = this.data.records.map(record => record.date);
-    this.saleTrace.y = this.data.records.map(record => record.salePrice);
+    ngOnInit() {
+        this.getData();
+    }
 
-    this.priceTraces = [this.bidTrace, this.saleTrace];
+    onSubmit() {
+        this.getData();
+    }
 
-  }
+    private getData() {
+        let body = {
+            currencyCode: this.filter.controls['currencyCode'].value,
+            startDate: this.filter.controls['startDate'].value,
+            endDate: this.filter.controls['endDate'].value
+        };
+        this.http.post<CurrencyResponseDto>(`${this.URL}`, body)
+            .subscribe(data => {
+                  this.data = data;
+                  this.createPricePlotTraces();
+                  this.createDifferencePlotTraces();
+                  this.layout.title = this.data.name + ' (' + this.filter.controls['currencyCode'].value + ')';
+                  this.error = false;
+              },
+              (error) => {
+                  this.error = true;
+                  this.errorMessage = error.error;
+              });
+    }
 
-  private createDifferencePlotTraces(){
+    private createPricePlotTraces() {
 
-    this.bidDifferenceTrace.x = this.data.recordsDifferences.map(record => record.date);
-    this.bidDifferenceTrace.y = this.data.recordsDifferences.map(record => record.bidPriceDifference);
+        this.bidTrace.x = this.data.records.map(record => record.date);
+        this.bidTrace.y = this.data.records.map(record => record.bidPrice);
 
-    this.saleDifferenceTrace.x = this.data.recordsDifferences.map(record => record.date);
-    this.saleDifferenceTrace.y = this.data.recordsDifferences.map(record => record.salePriceDifference);
+        this.saleTrace.x = this.data.records.map(record => record.date);
+        this.saleTrace.y = this.data.records.map(record => record.salePrice);
 
-    this.differenceTraces = [this.bidDifferenceTrace, this.saleDifferenceTrace];
+        this.priceTraces = [this.bidTrace, this.saleTrace];
+    }
 
-  }
+    private createDifferencePlotTraces(){
 
-  private getTodayDate(){
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0');
-    let yyyy = today.getFullYear();
-    return yyyy + '-' + mm + '-' + dd;
-  }
+        this.bidDifferenceTrace.x = this.data.recordsDifferences.map(record => record.date);
+        this.bidDifferenceTrace.y = this.data.recordsDifferences.map(record => record.bidPriceDifference);
 
-  private getYearBeforeDate(){
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0');
-    let yyyy = today.getFullYear() - 1;
-    return yyyy + '-' + mm + '-' + dd;
-  }
+        this.saleDifferenceTrace.x = this.data.recordsDifferences.map(record => record.date);
+        this.saleDifferenceTrace.y = this.data.recordsDifferences.map(record => record.salePriceDifference);
 
+        this.differenceTraces = [this.bidDifferenceTrace, this.saleDifferenceTrace];
+    }
+
+    private getTodayDate() {
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0');
+        let yyyy = today.getFullYear();
+
+        return yyyy + '-' + mm + '-' + dd;
+    }
+
+    private getYearBeforeDate() {
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0');
+        let yyyy = today.getFullYear() - 1;
+
+        return yyyy + '-' + mm + '-' + dd;
+    }
 }
